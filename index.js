df204aa6063c127eb79c219d5296c020358c00c8
Alexwolfdog <137944730+Alexwolfdog@users.noreply.github.com>
2023-12-17 19:14:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app=express();
 app.get("/",(req,res,next)=>{
   return "hello";
 });
+app.get('*', (req, res) => {
+  // Optionally, you can log the request if needed
+  console.log(`Ignoring request for: ${req.originalUrl}`);
+  // Respond with a 404 status (or any other response as needed)
+  res.status(404).send('Not Found');
+});
 const server=http.createServer(app)
 
 const port=process.env.PORT||5000;
