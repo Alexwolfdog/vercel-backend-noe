167d86fb5c2289e52dc5d29044b932e3876fe7e3
Alexwolfdog <137944730+Alexwolfdog@users.noreply.github.com>
2023-12-17 18:40:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express=require('express');
+const http=require('http')
 
+const app=express();
+app.use((req,res,next)=>{
+  return res.status(100).json("hello");
+});
+const server=http.createServer(app)
+
+server.listen(process.env.PORT,()=>{
+  console.log("server running");
+})
+
+module.export=server
